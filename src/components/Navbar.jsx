56892775c4293ca8273ba973017981b3bc7243f0
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-03-02 23:04:06 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,3 @@
-// import React from 'react'
-// import { NavLink } from 'react-router-dom'
-// import './navbar.css'
-// import { IoIosCart } from "react-icons/io";
-// import { useMenu } from '../context/Context';
-
-// function Navbar() {
-
-//   const {total_item} = useMenu();
-
-//   return (
-//     <>
-//     <div className="navbar">
-//         <div className="nav-logo">
-//             <h1>Cluster</h1>
-//         </div>
-
-//         <div className="nav-links">
-//             <NavLink to='/'>Home</NavLink> 
-//             <NavLink to='/about'>About</NavLink> 
-//             <NavLink to='/contact'>Contact</NavLink> 
-//             <NavLink to='/menu'>Menu</NavLink>
-//             <NavLink to='/cart'><IoIosCart className='nav-icon'/>
-//                 <span>{total_item}</span>
-//             </NavLink> 
-//         </div>
-//     </div>
-//     </>
-//   )
-// }
-
-// export default Navbar 
-
-
-
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
